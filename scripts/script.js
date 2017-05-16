3e3cd43f15b4cd98aaf457a93c6a897950882bf9
Greg Munker <gmunker@gmail.com>
2017-05-15 21:06:56 -0700

    stylings

--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,35 @@
 //   })
 // }
 
-
-
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+
+let toolbox = {
+  "html5_bar": "90%",
+  "css3_bar": "90%",
+  "javascript_bar": "90%",
+  jquery_bar: "90%",
+  sass_bar: "75%",
+  bootstrap_bar: "90%",
+  foundation_bar: "80%",
+  reactjs_bar: "75%",
+  redux_bar: "70%",
+  router_bar: "70%",
+  nodejs_bar: "40%",
+  webpack_bar: "70%",
+  git_bar: "70%",
+  yarn_bar: "90%",
+  npm_bar: "90%"
+};
+
+let progBarFill = () => {
+  for (let barVal in toolbox) {
+    $(`#${barVal}`).animate(
+      {width: toolbox[barVal]},
+      1000
+    );
+  }
+};
+
+progBarFill();
